10b95886108ca39cd3873f5631c0fbfec95e9e17
indebele00 <igietshibz@gmail.com>
2025-09-24 14:10:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/factor-effort-estimator/',   // <<-- change this line
+  base: '/factor-effort-estimator/',   
   plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 })
 
